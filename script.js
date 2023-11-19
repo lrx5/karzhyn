8e7f9ae90b852e456e57fa18e59c596b8779d37c
Lraxious <eghi.nolasco@gmail.com>
2023-11-20 03:33:37 +0800

    More bug fixes, Added thank_you.html

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ hamburger.addEventListener('click', (e)=> {
     navsmallmenu.classList.toggle('navsmallmenushow');
 });
 
+
 document.addEventListener('DOMContentLoaded', function() {
     // Assuming your form has an ID of 'myForm'
     document.getElementById('myForm').addEventListener('submit', function(event) {
@@ -17,8 +18,25 @@ document.addEventListener('DOMContentLoaded', function() {
         emailjs.sendForm('service_5yslqtc', 'template_10x7ix8', this)
             .then(function(response) {
                 console.log('SUCCESS!', response.status, response.text);
+                // Redirect to thank_you.html after a successful submission
+                window.location.href = 'thank_you.html';
             }, function(error) {
                 console.log('FAILED...', error);
             });
+    });
+    const images = document.querySelectorAll('.fade-in');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            console.log('Entry:', entry); // Log the intersection entry
+            if (entry.isIntersecting) {
+                entry.target.classList.toggle('fade-in');
+                entry.target.classList.toggle('is-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 }); // Adjust the threshold as needed
+    
+    images.forEach(image => {
+        observer.observe(image);
     });
 });
